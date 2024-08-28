dcf19e432df2880b824868d16be29a4f76f456f6
m.malmir <mahsa.malmir769@gmail.com>
2024-08-28 17:26:06 +0330

    fibonacci aalgorithm

---
+++ fibonacci.js
@@ -0,0 +1,12 @@
+// Fibonacci Numbers
+// [1,1,2,3,5,8,13,21,34,55,...]
+// fib(4) =5
+const fib = (n) => {
+    const numbers = [1, 1]
+    for (i = 2; i <= n; i++) {
+        numbers.push(numbers[i - 1] + numbers[i - 2])
+    }
+    return numbers[n]
+}
+
+console.log(fib(5), "fib 5"); //T=O(n) =>Linear Time Complxty
