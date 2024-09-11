f10acb973bd0f353b6172a26d258f9052ca7a5be
m.malmir <mahsa.malmir769@gmail.com>
2024-09-11 18:05:10 +0330

    add findMinimumValue alg

---
+++ find-minimum.js
@@ -0,0 +1,18 @@
+const arraySort = [2, 4, 5, 1, 6, 9]
+const arraySwap = [2, 4, 5, 1, 6, 9]
+function findMinimumValueSort(a) {
+    a.sort() 
+    return a[0]
+}
+
+function findMinimumValueSwap(a) {
+ if(i =0 , i< a.lenght ,i++){
+    
+ }
+}
+
+//when we change a array inside of a function outside array
+// value is change becuse any array in memort have a special and uniq space
+console.log(findMinimumValueSort(arraySort)); //O(1)
+console.log(findMinimumValueSwap(arraySwap)); //O(n)
+console.log(array ,"array");
