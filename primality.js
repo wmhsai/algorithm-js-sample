a091942abdcd6f5255d79dfad69fd2860d5d5bad
m.malmir <mahsa.malmir769@gmail.com>
2024-09-11 17:43:58 +0330

    add  Math.sqrt(number)   isPrime algorithm or bettertime complexity

--- primality.js
+++ primality.js
@@ -1,10 +1,10 @@
 //Primality Test
 // 5 is prime => 1*5
 // 9 is not prime => 1*9 || 3*3
-//try dividing the number by all smaller numbers and return true if its only divisible by itself and 1.
+//try dividing the number by all smaller   and return true if its only divisible by itself and 1.
 
 function isPrime(number) {
-    for (let i = 2; i < number; i++) {
+    for (let i = 2; i < number; i++) { //n
         if (number % i === 0) {
             return false
         }
@@ -12,5 +12,18 @@ function isPrime(number) {
     return true
 }
 
+// Best Case : number =1 OR number =2 =>O(1)
+// Average Case: O(n)
+//worst Case : number = 27,277 => O(n)
 console.log(isPrime(5), "is prime 5");
 console.log(isPrime(9), "is prime 9");
+
+
+function isPrimeEfficient(number) {
+    for (let i = 2; i < Math.sqrt(number); i++) { // جذر  O(n) (Improve :O(sqrt(n)) )
+        if (number % i === 0) {
+            return false
+        }
+    }
+    return true
+}
