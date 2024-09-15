752c25baa575170a2dcb0d80bd4cbef226792224
m.malmir <mahsa.malmir769@gmail.com>
2024-09-15 17:13:17 +0330

    isEven algorithme

---
+++ isEven.js
@@ -0,0 +1,13 @@
+// function isEven(number) {
+//     if (number % 2 === 0) {
+//         return true //even
+//     }
+//     else
+//         return false //odd
+// }
+function isEven(number) {
+    return number % 2 === 0
+}
+
+console.log(isEven(3), "is even"); // T=O(1)
+console.log(isEven(2), "is even"); 
