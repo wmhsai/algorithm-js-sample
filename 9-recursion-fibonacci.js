7de8427fa162da1ae0e6fe7cd60936d009b961ed
m.malmir <mahsa.malmir769@gmail.com>
2024-10-06 18:06:25 +0330

    fibDynamicProgrammer

--- 9-recursion-fibonacci.js
+++ 9-recursion-fibonacci.js
@@ -1,12 +1,25 @@
+// [1,1,2,3,5,8,13,21,...] 
 
 const fib = (n) => {
-    if (n === 1 || n === 1) {
-        return 1
-    }
-    for (i = 2; i <= n; i++) {
-        numbers.push(numbers[i - 1] + numbers[i - 2])
-    }
-    return numbers[n]
-}
+    if (n === 0 || n === 1) return 1
 
-console.log(fib(5), "fib 5"); //T=O(n) =>Linear Time Complxty
+    return fib(n - 1) + fib(n - 2);
+}
+console.log(fib(5), "fib 5");
+//T=O(2^n) =>Loop-base solution
+
+const fibDynamicProgrammer = (n, mem) => {
+    let res;
+    if (mem[n]) return mem[n]
+
+    if (n == 0 || n == 1) {
+        res = 1
+    }
+    else {
+        res = fib(n - 1, mem) + fib(n - 2, mem);
+    }
+    mem[n] = res;
+    return res;
+}
+console.log(fib(5, {}), "fib 5");
+//T=O(2*n)  => T=O(n) =>Linear Time Complxty
