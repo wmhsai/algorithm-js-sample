979ae84be3383415cedb805642b9920f4c09385f
m.malmir <mahsa.malmir769@gmail.com>
2024-11-06 16:07:27 +0330

    BubbleSort algorithme

---
+++ 11-bubbleSortAlgorithme.js
@@ -0,0 +1,25 @@
+//Bubble sort 
+function BubbleSort(array) {
+    const resultArray = [...array]
+    for (let outer = 0; outer < resultArray.length; outer++) {
+        let outerEl = resultArray[outer];
+        for (let inner = outer + 1; inner < resultArray.length; inner++) {
+            let innerEl = resultArray[inner]
+            if (outerEl > innerEl) {
+                // if (outerEl < innerEl) { //descending order
+                resultArray[outer] = innerEl;
+                resultArray[inner] = outerEl; //after swap elements
+                outerEl = resultArray[outer] //update our refrences
+                innerEl = resultArray[inner]
+
+            }
+        }
+    }
+    return resultArray
+}
+const sortedArray = BubbleSort([5, 10, -3, -10, 1, 100, 99])
+console.log(sortedArray, "BubbleSortArray");
+// Time Complexity
+//  in best case is O(n)
+//  in average case is Tends to be O(n^2)
+//  in worst case is O(n^2)
